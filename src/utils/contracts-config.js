1ed4af8146540550cbe0602d1183ff2d3fe75309
Aymen <AymenMir1001@gmail.com>
2022-06-10 00:17:58 +0100

    initial commit

---
+++ src/utils/contracts-config.js
@@ -0,0 +1,5 @@
+
+  export const contractAddress = "0x49d830751815f5c33C6f631E2381F4E6eD3d6708"
+  export const ownerAddress = "0x2d32de51e69EeBf594EB21Eb97903E9dF1C5C7BB"
+  export const networkDeployedTo = "1337"
+  
