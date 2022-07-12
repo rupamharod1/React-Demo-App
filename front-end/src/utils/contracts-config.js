3f1912c55e5d6ee1faaa13c6bc3a2152f3d2e8dc
Aymen <AymenMir1001@gmail.com>
2022-07-12 23:57:43 +0100

    front-end & smart contracts updated

---
+++ front-end/src/utils/contracts-config.js
@@ -0,0 +1,5 @@
+
+    export const contractAddress = "0xD225E53Fc59Dc8481046cdaf4B8AcA85cE50A1EE"
+    export const ownerAddress = "0xE1799E68DE44b631c953EFD391A0007e5Ee2Ce5f"
+    export const networkDeployedTo = "1337"
+    
